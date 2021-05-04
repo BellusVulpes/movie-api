50ec88547da1c7c399a619cb92a04cb6e0e9cb11
christopher boucher <chrismichb@yahoo.com>
2021-05-03 21:39:57 -0400

    added index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const movies = require('./movies.js')
+const { getAllData, movieData, addNewMovie } = require('./controller/movie')
 
 const app = express()
+
+app.get('/', getAllData)
+
+app.get('/movies/:movieData', movieData)
+
+app.use(bodyParser.json())
+
+app.post('/movies', addNewMovie)
+
+app.all('*'), (request, response) => {
+  return request.sendStatus(404)
+}
 
 app.listen(8080, () => {
   console.log('Listening on port 8080...') // eslint-disable-line no-console
